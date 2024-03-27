10a31a014b4f5e31cc6c9d3602ffbc014d45af5d
Gabriel Zanon <gabrielzanon77@outlook.com>
2024-03-27 19:13:19 -0300

    feat(mappings): adicionando novas keymaps para o Neogit e LazyGit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,16 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+-- Keymaps Gerais
 map("i", "jk", "<ESC>")
+map("n", "<leader>X", "<cmd> confirm qa <cr>", { desc = "Close all buffers" })
 
-map("n", "<leader>X", "<cmd> confirm qa <cr>")
+-- Neogit
+map("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Open Neogit" })
+map("n", "<leader>gc", "<cmd>Neogit commit<cr>", { desc = "Neogit Commit" })
+map("n", "<leader>gp", "<cmd>Neogit pull<cr>", { desc = "Neogit Pull" })
+map("n", "<leader>gP", "<cmd>Neogit push<cr>", { desc = "Neogit Push" })
 
+-- LazyGit
+map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
